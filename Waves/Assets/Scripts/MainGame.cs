44b35ff507c9a99c84998c173d2e8f0bba468e1b
Keith Morgado <keith.morgado@gmail.com>
2017-01-21 15:44:02 -0500

    Adding Main Game script

---
+++ Waves/Assets/Scripts/MainGame.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainGame : MonoBehaviour {
+
+    KanyeController kanye;
+
+	// Use this for initialization
+	void Start () {
+        kanye = GameObject.Find("Kanye").GetComponent<KanyeController>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            //Move Character
+            kanye.move(new Vector3(2, 0));
+        }
+	}
+}
