d455f8dc22a7c2641929711e9ba8794677c2d576
Joshua Johnson <johnson.joshua.work@gmail.com>
2017-01-21 17:18:57 -0500

    change mass based on key down

--- Waves/Assets/Scripts/MainGame.cs
+++ Waves/Assets/Scripts/MainGame.cs
@@ -5,20 +5,25 @@ using UnityEngine;
 public class MainGame : MonoBehaviour {
 
     KanyeController kanye;
+    GenerateTerrain waveTerrain;
 
 	// Use this for initialization
 	void Start () {
         kanye = GameObject.Find("Kanye").GetComponent<KanyeController>();
+        waveTerrain = GameObject.Find("Wave").GetComponent<GenerateTerrain>();
     }
 	
 	// Update is called once per frame
 	void Update () {
 		
 
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(KeyCode.D))
         {
-            //Move Character
-            kanye.move(new Vector3(2, 0));
+            kanye.rigidBody2D.mass = 2.5f;
+        }
+        else
+        {
+            kanye.rigidBody2D.mass = 1;
         }
 	}
 }
