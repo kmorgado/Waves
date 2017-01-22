9799431e5f4b1c14681b1f4eb9cda0d269a62486
EVERDRED\Keith <keith.morgado@gmail.com>
2017-01-21 23:30:44 -0500

    SFX

--- Waves/Assets/Scripts/Oxygen.cs
+++ Waves/Assets/Scripts/Oxygen.cs
@@ -4,22 +4,33 @@ using System.Linq;
 using System.Text;
 using UnityEngine;
 
-   public class Oxygen : MonoBehaviour
+public class Oxygen : MonoBehaviour
+{
+
+    #region Audio
+
+    public AudioClip almostDead;
+    public AudioClip dead;
+
+    #endregion
+
+    public GameObject OxygenBar;
+
+    public AudioSource audio1;
+
+    float MaxOxygen = 100;
+    float OxygenLevel = 100;
+
+
+    bool playAlmostDead = true;
+
+    public void AddOxygen(float value)
     {
+        OxygenLevel += value;
 
-        public GameObject OxygenBar;
-
-        float MaxOxygen = 100;
-        float OxygenLevel = 100;
-
-
-        public void AddOxygen(float value)
-        {
-            OxygenLevel += value;
-
-            if (OxygenLevel >= MaxOxygen)
-                OxygenLevel = MaxOxygen;
-        }
+        if (OxygenLevel >= MaxOxygen)
+            OxygenLevel = MaxOxygen;
+    }
 
     public void IncreaseOxygen()
     {
@@ -27,22 +38,45 @@ using UnityEngine;
 
         if (OxygenLevel >= MaxOxygen)
             OxygenLevel = MaxOxygen;
+
+        if(OxygenLevel > 80)
+        {
+            playAlmostDead = true;
+            audio1.PlayOneShot(dead);
+
+        }
     }
 
     public void DecreaseOxygen()
-        {
-            OxygenLevel -= .2f;
+    {
+        OxygenLevel -= .2f;
 
-            if (OxygenLevel <= 0)
-            {
-                //Game over
-                OxygenLevel = 0;
+        if (OxygenLevel <= 30)
+        {
+            if (playAlmostDead)
+            { 
+            audio1.PlayOneShot(almostDead);
+                playAlmostDead = false;
             }
         }
 
-        void Update()
+        if (OxygenLevel <= 0)
         {
-            OxygenBar.transform.localScale = new Vector3(OxygenBar.transform.localScale.x, OxygenLevel / 100.0f, OxygenBar.transform.localScale.z);
-        }
 
+            OxygenLevel = 0;
+
+            Debug.Log("DEAD");
+            //Game over
+            if(!audio1.isPlaying)
+                audio1.PlayOneShot(dead);
+
+        }
     }
+
+    void Update()
+    {
+
+        OxygenBar.transform.localScale = new Vector3(OxygenBar.transform.localScale.x, OxygenLevel / 100.0f, OxygenBar.transform.localScale.z);
+    }
+
+}
